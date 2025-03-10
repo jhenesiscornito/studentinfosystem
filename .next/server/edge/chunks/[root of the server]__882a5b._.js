41445384d2ee48a534710a88867064a74c3b0ce2
Jhenesis D. Cornito <jhenesiscornito@gmail.com>
2025-03-10 09:35:20 +0800

    please gana

--- .next/server/edge/chunks/[root of the server]__882a5b._.js
+++ .next/server/edge/chunks/[root of the server]__882a5b._.js
@@ -22,12 +22,25 @@ module.exports = mod;
 var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, z: __turbopack_require_stub__ } = __turbopack_context__;
 {
 __turbopack_esm__({
-    "config": (()=>config)
+    "config": (()=>config),
+    "middleware": (()=>middleware)
 });
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$esm$2f$server$2f$web$2f$spec$2d$extension$2f$response$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next/dist/esm/server/web/spec-extension/response.js [middleware] (ecmascript)");
 ;
+;
+function middleware(req) {
+    const res = __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$esm$2f$server$2f$web$2f$spec$2d$extension$2f$response$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["NextResponse"].next();
+    res.headers.append("Access-Control-Allow-Origin", "*");
+    res.headers.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.headers.append("Access-Control-Allow-Headers", "Content-Type");
+    return res;
+}
 const config = {
     matcher: [
         "/dashboard"
+    ],
+    matcher: [
+        "/api/:path*"
     ]
 };
 }}),
@@ -38,6 +51,7 @@ var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_
 {
 __turbopack_esm__({});
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2d$auth$2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next-auth/middleware.js [middleware] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$esm$2f$api$2f$server$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$module__evaluation$3e$__ = __turbopack_import__("[project]/node_modules/next/dist/esm/api/server.js [middleware] (ecmascript) <module evaluation>");
 var __TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_import__("[project]/middleware.js [middleware] (ecmascript) <locals>");
 }}),
 "[project]/middleware.js [middleware] (ecmascript) <exports>": ((__turbopack_context__) => {
@@ -47,7 +61,8 @@ var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_
 {
 __turbopack_esm__({
     "config": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$locals$3e$__["config"]),
-    "default": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2d$auth$2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["default"])
+    "default": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2d$auth$2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["default"]),
+    "middleware": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$locals$3e$__["middleware"])
 });
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2d$auth$2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next-auth/middleware.js [middleware] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_import__("[project]/middleware.js [middleware] (ecmascript) <locals>");
@@ -59,7 +74,8 @@ var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_
 {
 __turbopack_esm__({
     "config": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$exports$3e$__["config"]),
-    "default": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$exports$3e$__["default"])
+    "default": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$exports$3e$__["default"]),
+    "middleware": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$exports$3e$__["middleware"])
 });
 var __TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$module__evaluation$3e$__ = __turbopack_import__("[project]/middleware.js [middleware] (ecmascript) <module evaluation>");
 var __TURBOPACK__imported__module__$5b$project$5d2f$middleware$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__$3c$exports$3e$__ = __turbopack_import__("[project]/middleware.js [middleware] (ecmascript) <exports>");
