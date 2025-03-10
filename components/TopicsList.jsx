d4cb4c73d4872b1d3bb0f4607fa99f7231a46406
Jhenesis D. Cornito <jhenesiscornito@gmail.com>
2025-03-10 09:02:51 +0800

    use react

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,54 +1,4 @@
-// import Link from "next/link";
-// import RemoveBtn from "./RemoveBtn";
-// import { HiPencilAlt } from "react-icons/hi";
-
-// const getTopics = async () => {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/topics", {
-//       cache: "no-store",
-//     });
-
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch topics");
-//     }
-
-//     return res.json();
-//   } catch (error) {
-//     console.log("Error loading topics: ", error);
-//   }
-// };
-
-// export default async function TopicsList() {
-//   const { topics } = await getTopics();
-
-//   return (
-//     <>
-//       {topics.map((t) => (
-//         <div
-//           key={t._id}
-//           className="p-4 border my-3 flex justify-between gap-5 items-start shadow-lg rounded-lg border-t-4 border-green-400"
-//         >
-//           <div>
-//             <h2 className="font-bold text-2xl">{t.title}</h2>
-//             <div>Department: {t.description}</div>
-//             <div>Year level: {t.year}</div>
-//             <div>Email: {t.email}</div>
-//             <div>Contact Number: {t.contact}</div>
-//           </div>
-
-//           <div className="flex gap-2">
-//             <RemoveBtn id={t._id} />
-//             <Link href={`/editTopic/${t._id}`}>
-//               <HiPencilAlt size={24} />
-//             </Link>
-//           </div>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
-// "use client";
+"use client";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -110,3 +60,55 @@ export default function TopicsList() {
     </>
   );
 }
+
+
+
+// import Link from "next/link";
+// import RemoveBtn from "./RemoveBtn";
+// import { HiPencilAlt } from "react-icons/hi";
+
+// const getTopics = async () => {
+//   try {
+//     const res = await fetch("http://localhost:3000/api/topics", {
+//       cache: "no-store",
+//     });
+
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch topics");
+//     }
+
+//     return res.json();
+//   } catch (error) {
+//     console.log("Error loading topics: ", error);
+//   }
+// };
+
+// export default async function TopicsList() {
+//   const { topics } = await getTopics();
+
+//   return (
+//     <>
+//       {topics.map((t) => (
+//         <div
+//           key={t._id}
+//           className="p-4 border my-3 flex justify-between gap-5 items-start shadow-lg rounded-lg border-t-4 border-green-400"
+//         >
+//           <div>
+//             <h2 className="font-bold text-2xl">{t.title}</h2>
+//             <div>Department: {t.description}</div>
+//             <div>Year level: {t.year}</div>
+//             <div>Email: {t.email}</div>
+//             <div>Contact Number: {t.contact}</div>
+//           </div>
+
+//           <div className="flex gap-2">
+//             <RemoveBtn id={t._id} />
+//             <Link href={`/editTopic/${t._id}`}>
+//               <HiPencilAlt size={24} />
+//             </Link>
+//           </div>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
