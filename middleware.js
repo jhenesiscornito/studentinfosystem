eb4a37ef1e81ab62c53d65288407bc5fd55cf6ec
Jhenesis D. Cornito <jhenesiscornito@gmail.com>
2025-03-10 09:31:52 +0800

    please

--- middleware.js
+++ middleware.js
@@ -1,3 +1,15 @@
 export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export const config = { matcher: ["/dashboard"] };
+export function middleware(req) {
+  const res = NextResponse.next();
+  res.headers.append("Access-Control-Allow-Origin", "*");
+  res.headers.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.headers.append("Access-Control-Allow-Headers", "Content-Type");
+  return res;
+}
+
+export const config = { 
+    matcher: ["/dashboard"],
+    matcher: ["/api/:path*"],
+};
